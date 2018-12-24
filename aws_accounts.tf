de4b57696a26c550ca6f8293015b98b219d26663
Jason Barto <jason.p.barto@gmail.com>
2018-12-24 06:46:06 +0000

    Update aws_accounts.tf

--- aws_accounts.tf
+++ aws_accounts.tf
@@ -2,12 +2,12 @@ module "sandbox_account" {
   source = "modules/account"
 
   account_name = "sandbox"
-  target_account_role_arn = "arn:aws:iam::776347453069:role/OrganizationAccountAccessRole"
+  target_account_role_arn = "arn:aws:iam::0123456789019:role/OrganizationAccountAccessRole"
 }
 
 module "dev_account" {
   source = "modules/account"
 
   account_name = "dev"
-  target_account_role_arn = "arn:aws:iam::965078485732:role/OrganizationAccountAccessRole"
+  target_account_role_arn = "arn:aws:iam::012345678912:role/OrganizationAccountAccessRole"
 }
